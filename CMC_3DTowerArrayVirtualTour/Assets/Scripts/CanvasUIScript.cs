b517cdc8e8e9b7ca5af2dd347e1fda4c1e717100
Chan Dong Ha <158008421+charlie-ha@users.noreply.github.com>
2025-06-28 16:12:30 -0400

    vr project

--- CMC_3DTowerArrayVirtualTour/Assets/Scripts/CanvasUIScript.cs
+++ CMC_3DTowerArrayVirtualTour/Assets/Scripts/CanvasUIScript.cs
@@ -1,7 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CanvasUIScript : MonoBehaviour
 {
+    public GameObject prefab;
     public void DestroyGameObject(GameObject thisGameObject)
     {
         Destroy(thisGameObject);
@@ -9,5 +12,10 @@ public class CanvasUIScript : MonoBehaviour
     public void EnableRaycast(bool boolean)
     {
         PlayerRayCast.instance.enabled = boolean;
+    }
+
+    public void SpawnPrefab()// spawn prefab/images/videos for VR project
+    {
+        Instantiate(prefab, new Vector3(8f,301.7f, -4f), Quaternion.identity);
     }
 }
--- CMC_3DTowerArrayVirtualTour/Assets/Scripts/HotspotScript.cs
+++ CMC_3DTowerArrayVirtualTour/Assets/Scripts/HotspotScript.cs
@@ -8,17 +8,5 @@ public class HotspotScript : MonoBehaviour
 
     public bool isSpawningPrefab =false;//spawn video/text/image
     public GameObject prefabToSpawn;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-        
-    }
+     
 }
