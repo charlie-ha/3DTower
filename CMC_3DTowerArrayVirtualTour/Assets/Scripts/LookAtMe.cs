2994777dccd50e283c5162372cfcbf17805002f9
Chan Dong Ha <158008421+charlie-ha@users.noreply.github.com>
2025-06-28 22:07:53 -0400

    mobile

--- CMC_3DTowerArrayVirtualTour/Assets/Scripts/LookAtMe.cs
+++ CMC_3DTowerArrayVirtualTour/Assets/Scripts/LookAtMe.cs
@@ -4,11 +4,7 @@ using UnityEngine;
 
 public class LookAtMe : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    
 
     // Update is called once per frame
     void Update()
